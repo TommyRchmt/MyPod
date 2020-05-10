0d4a01a3effce20b6eac0ab1d4cd7a8e06bc6d3f
Tommy Rachmat <tommy.rachmat@amarbank.co.id>
2020-05-10 17:51:27 +0700

    Initial Commit

---
+++ MyPod/MyPod.h
@@ -0,0 +1,19 @@
+//
+//  MyPod.h
+//  MyPod
+//
+//  Created by AMARBANK on 10/05/20.
+//  Copyright © 2020 Test. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyPod.
+FOUNDATION_EXPORT double MyPodVersionNumber;
+
+//! Project version string for MyPod.
+FOUNDATION_EXPORT const unsigned char MyPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyPod/PublicHeader.h>
+
+
---
+++ MyPod/MyPod.swift
@@ -0,0 +1,16 @@
+//
+//  MyPod.swift
+//  MyPod
+//
+//  Created by AMARBANK on 10/05/20.
+//  Copyright © 2020 Test. All rights reserved.
+//
+
+import Foundation
+
+public class MyPod {
+    public static func printSomething() -> String {
+        let myWord = "String berhasil di print."
+        return myWord
+    }
+}
